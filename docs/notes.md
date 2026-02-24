ab718a01340acaa7cebb7e55f7cd44f083d2b36e
Alex Young <alexyoung0119@gmail.com>
2026-02-24 00:17:14 -0600

    OK let me cook; ReLU, sigmoid, and softmax activations impl

--- docs/notes.md
+++ docs/notes.md
@@ -1,3 +1,15 @@
 Back propagation:
 dL (gradient of loss) w.r.t y (dL/dy) is passed backwards from final (outout) layer
 use this to calculate dL/dW, dL/dB, dL/dx
+
+its all chain rule... dL/dx = dL/dy * dy/dx
+                               "g"
+                d Loss w.r.t inout
+                                deriv. of activation
+
+activation functions:
+need these to increase model complexity (otherwise, all layers collapse to a linear combination, like a linear
+regression)
+these sit in between layers, non-linearly transforming raw layer input to feed forward
+back propagation works the same; recieve the gradient w.r.t its output (dL/dy), use this to find dL/dx, to continue
+passing backward. see above for finding dL/dx
--- src/activation.rs
+++ src/activation.rs
@@ -1,11 +1,86 @@
 pub trait Activation {
+    /// Parameters:
+    /// [input]: vector of output from previous layer
     fn forward(&mut self, input: &[f64]) -> Vec<f64>;
-    fn backward(&mut self, grad_input: &[f64]) -> Vec<f64>;
+
+    /// Parameters:
+    /// [grad_output]: gradient vector w.r.t. this activation's output
+    fn backward(&mut self, grad_output: &[f64]) -> Vec<f64>;
+
     fn zero_grad(&mut self) {}
 }
 
 pub struct ReLU {
+    // d/dx = 1 iff x > 0
     output: Option<Vec<f64>>,
 }
 
-impl Activation for ReLU {}
+impl Activation for ReLU {
+    fn forward(&mut self, input: &[f64]) -> Vec<f64> {
+        let output: Vec<f64> = input.iter().map(|&x| x.max(0.0)).collect();
+        self.output = Some(output.clone());
+        output
+    }
+
+    fn backward(&mut self, grad_output: &[f64]) -> Vec<f64> {
+        let output = self
+            .output
+            .take()
+            .expect("Forward must be called before backward");
+
+        grad_output
+            .iter()
+            .zip(&output)
+            .map(|(&g, &o)| g * if o > 0.0 { 1.0 } else { 0.0 })
+            .collect()
+    }
+}
+
+pub struct SoftMax {
+    // d/dx = class.exp() / classes.exp().sum()
+    output: Option<Vec<f64>>,
+}
+
+impl Activation for SoftMax {
+    fn forward(&mut self, input: &[f64]) -> Vec<f64> {
+        // subtract all input from max for numerical stability
+        let max_val = input.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
+        let exp_sum: f64 = input.iter().map(|&x| (x - max_val).exp()).sum();
+        let output: Vec<f64> = input
+            .iter()
+            .map(|&x| (x - max_val).exp() / exp_sum)
+            .collect();
+        self.output = Some(output.clone());
+        output
+    }
+
+    fn backward(&mut self, _grad_output: &[f64]) -> Vec<f64> {
+        unimplemented!("Use CrossEntropy loss instead");
+    }
+}
+
+pub struct Sigmoid {
+    // d/dx = output (1 - output)
+    output: Option<Vec<f64>>,
+}
+
+impl Activation for Sigmoid {
+    fn forward(&mut self, input: &[f64]) -> Vec<f64> {
+        let output: Vec<f64> = input.iter().map(|x| 1.0 / (1.0 + (-x).exp())).collect();
+        self.output = Some(output.clone());
+        output
+    }
+
+    fn backward(&mut self, grad_output: &[f64]) -> Vec<f64> {
+        let output = self
+            .output
+            .take()
+            .expect("Forward must be called before backward");
+
+        grad_output
+            .iter()
+            .zip(&output)
+            .map(|(&g, &o)| g * o * (1.0 - o))
+            .collect()
+    }
+}
