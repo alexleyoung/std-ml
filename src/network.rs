9e303b59e42a1562f03dc5b59558eb1c2f249ca8
Alex Young <alexyoung0119@gmail.com>
2026-03-03 00:52:34 -0600

    network first pass

---
+++ src/network.rs
@@ -0,0 +1,41 @@
+use crate::Layer;
+
+pub struct Network {
+    layers: Vec<Box<dyn Layer>>,
+}
+
+impl Network {
+    pub fn new() -> Self {
+        Self { layers: vec![] }
+    }
+    pub fn add_layer(&mut self, layer: Box<dyn Layer>) {
+        self.layers.push(layer);
+    }
+
+    pub fn forward(&mut self, input: &[f64]) -> Vec<f64> {
+        let mut out = input.to_vec();
+        for layer in &mut self.layers {
+            out = layer.forward(&out);
+        }
+        out
+    }
+
+    pub fn backward(&mut self, grad_output: &[f64]) {
+        let mut dx = grad_output.to_vec();
+        for layer in self.layers.iter_mut().rev() {
+            dx = layer.backward(&dx);
+        }
+    }
+
+    pub fn update(&mut self, learning_rate: f64) {
+        for layer in &mut self.layers {
+            layer.update(learning_rate);
+        }
+    }
+
+    pub fn zero_grad(&mut self) {
+        for layer in &mut self.layers {
+            layer.zero_grad();
+        }
+    }
+}
