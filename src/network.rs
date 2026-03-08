ff92d9f26c0d43b8208f2d285540db208443e5d2
Alex Young <alexyoung0119@gmail.com>
2026-03-07 21:58:39 -0600

    start testing

--- src/network.rs
+++ src/network.rs
@@ -8,6 +8,7 @@ impl Network {
     pub fn new() -> Self {
         Self { layers: vec![] }
     }
+
     pub fn add_layer(&mut self, layer: Box<dyn Layer>) {
         self.layers.push(layer);
     }
@@ -20,8 +21,8 @@ impl Network {
         out
     }
 
-    pub fn backward(&mut self, grad_output: &[f64]) {
-        let mut dx = grad_output.to_vec();
+    pub fn backward(&mut self, grd_output: &[f64]) {
+        let mut dx = grd_output.to_vec();
         for layer in self.layers.iter_mut().rev() {
             dx = layer.backward(&dx);
         }
@@ -37,5 +38,36 @@ impl Network {
         for layer in &mut self.layers {
             layer.zero_grad();
         }
+    }
+
+    pub fn layer_count(&self) -> usize {
+        self.layers.len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::layer::Linear;
+
+    fn create_n_layer_network(n: usize) -> Network {
+        let mut net = Network::new();
+        (0..n)
+            .into_iter()
+            .for_each(|_| net.add_layer(Box::new(Linear::new(3, 3))));
+        net
+    }
+
+    #[test]
+    fn test_add_layer() {
+        let mut net = Network::new();
+        let linear = Linear::new(5, 5);
+        net.add_layer(Box::new(linear));
+        assert!(net.layers.len() == 1);
+    }
+
+    #[test]
+    fn test_forward() {
+        let mut net = create_n_layer_network(5);
     }
 }
