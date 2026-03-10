a822a160f8c4d1e326d3e4b99501add935d3d056
Alex Young <alexyoung0119@gmail.com>
2026-03-09 23:31:13 -0500

    mse loss/grad

--- src/loss.rs
+++ src/loss.rs
@@ -1,28 +1,39 @@
 pub trait Loss {
-    fn loss(&self, yhat: &[f64], y: &[f64]) -> f64;
-    fn gradient(&self, yhat: &[f64], y: &[f64]) -> Vec<f64>;
+    fn loss(&self, p: &[f64], y: &[f64]) -> f64;
+    fn gradient(&self, p: &[f64], y: &[f64]) -> Vec<f64>;
 }
 
 pub struct MSE {}
 
 impl Loss for MSE {
-    fn loss(&self, yhat: &[f64], y: &[f64]) -> f64 {
-        todo!()
+    fn loss(&self, p: &[f64], y: &[f64]) -> f64 {
+        let n = p.len();
+        assert!(n == y.len(), "Prediction size should match truth size");
+
+        let mut loss = 0.0;
+        p.iter()
+            .zip(y.iter())
+            .for_each(|(&p, &y)| loss += (p - y).powf(2.0));
+        loss / n as f64
     }
 
-    fn gradient(&self, yhat: &[f64], y: &[f64]) -> Vec<f64> {
-        todo!()
+    fn gradient(&self, p: &[f64], y: &[f64]) -> Vec<f64> {
+        let n = p.len() as f64;
+        p.iter()
+            .zip(y.iter())
+            .map(|(&p, &y)| 2.0 / n * (p - y))
+            .collect()
     }
 }
 
 pub struct CrossEntropy {}
 
 impl Loss for CrossEntropy {
-    fn loss(&self, yhat: &[f64], y: &[f64]) -> f64 {
+    fn loss(&self, p: &[f64], y: &[f64]) -> f64 {
         todo!()
     }
 
-    fn gradient(&self, yhat: &[f64], y: &[f64]) -> Vec<f64> {
+    fn gradient(&self, p: &[f64], y: &[f64]) -> Vec<f64> {
         todo!()
     }
 }
