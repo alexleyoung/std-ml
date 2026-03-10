460e3d8cc5ec0a48db73e37cb636e5823f708601
Alex Young <alexyoung0119@gmail.com>
2026-03-09 23:34:00 -0500

    proper assert

--- src/loss.rs
+++ src/loss.rs
@@ -7,18 +7,26 @@ pub struct MSE {}
 
 impl Loss for MSE {
     fn loss(&self, p: &[f64], y: &[f64]) -> f64 {
-        let n = p.len();
-        assert!(n == y.len(), "Prediction size should match truth size");
+        let n = p.len() as f64;
+        assert!(
+            n as usize == y.len(),
+            "Prediction size should match truth size"
+        );
 
         let mut loss = 0.0;
         p.iter()
             .zip(y.iter())
             .for_each(|(&p, &y)| loss += (p - y).powf(2.0));
-        loss / n as f64
+        loss / n
     }
 
     fn gradient(&self, p: &[f64], y: &[f64]) -> Vec<f64> {
         let n = p.len() as f64;
+        assert!(
+            n as usize == y.len(),
+            "Prediction size should match truth size"
+        );
+
         p.iter()
             .zip(y.iter())
             .map(|(&p, &y)| 2.0 / n * (p - y))
