6dfe263604c8bdc26377b28c8631a601f13bc7cf
Alex Young <alexyoung0119@gmail.com>
2026-03-28 17:20:57 -0500

    main example skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std_ml::{
+    activation::ReLU,
+    layer::Linear,
+    loader::DataLoader,
+    loss::{CrossEntropy, Loss},
+    network::Network,
+};
+
 fn main() {
-    println!("hello world!");
+    let mut net = Network::new();
+    net.add_layer(Box::new(Linear::new(784, 256)));
+    net.add_layer(Box::new(ReLU::new()));
+    net.add_layer(Box::new(Linear::new(256, 10)));
+    let loss_fn = CrossEntropy {};
+
+    let dataloader = DataLoader::new("", "");
+
+    for (data, targets) in dataloader {
+        let out = net.forward(data);
+        loss_fn.loss(&out, targets)
+    }
 }
