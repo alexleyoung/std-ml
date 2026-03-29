e1234fc5a5e2c73760addae07923b9a192869071
Alex Young <alexyoung0119@gmail.com>
2026-03-28 20:22:46 -0500

    fix data paths in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std_ml::{
     activation::ReLU,
     layer::Linear,
-    loader::DataLoader,
+    loader::IDXDataLoader,
     loss::{CrossEntropy, Loss},
     network::Network,
 };
@@ -13,10 +13,23 @@ fn main() {
     net.add_layer(Box::new(Linear::new(256, 10)));
     let loss_fn = CrossEntropy {};
 
-    let dataloader = DataLoader::new("", "");
+    let dataloader = IDXDataLoader::new(
+        "data/MNIST/raw/train-images-idx3-ubyte",
+        "data/MNIST/raw/train-labels-idx1-ubyte",
+        64,
+    );
 
-    for (data, targets) in dataloader {
-        let out = net.forward(data);
-        loss_fn.loss(&out, targets)
+    for (batch_idx, (data, targets)) in dataloader.iter().enumerate() {
+        net.zero_grad();
+        let num_samples = data.len() / 784;
+        for i in 0..num_samples {
+            let sample = &data[i * 784..(i + 1) * 784];
+            let label = targets[i] as usize;
+            let mut one_hot = vec![0.0; 10];
+            one_hot[label] = 1.0;
+            let out = net.forward(sample);
+            loss_fn.loss(&out, &one_hot);
+        }
+        println!("batch {batch_idx}: {num_samples} samples");
     }
 }
